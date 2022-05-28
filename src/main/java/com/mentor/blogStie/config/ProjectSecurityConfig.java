f7ca42fdc0f94228c21ac89fbe1fec2a5ef9b3bf
Anwr (EXT) <amranw79@mgc.mentorg.com>
2022-05-28 20:13:16 +0200

    second commit

--- src/main/java/com/mentor/blogStie/config/ProjectSecurityConfig.java
+++ src/main/java/com/mentor/blogStie/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -23,12 +24,8 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().and().httpBasic();
     }
 
-    /*@Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
